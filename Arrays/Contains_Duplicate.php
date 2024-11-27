7068885dbfab6b33efc9213d2f08cfa118f45e17
ameur <ameurelmoukh04@gmail.com>
2024-11-27 18:36:28 +0100

    commited

--- Arrays/Contains_Duplicate.php
+++ Arrays/Contains_Duplicate.php
@@ -2,8 +2,20 @@
 
 
 class Solution {
-
-    function containsDuplicate($nums) {
+#first solution I used two nested loops (bruteforce Method)
+function containsDuplicateBruteForce($nums){
+    $n = count($nums);
+    for($i = 0;$i < $n ;$i++){
+        for($j =$i+1 ;$j < $n; $j++){
+            if($nums[$i] == $nums[$j]){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+#second solution, I used the isset method to check a value if it already exists in the values array
+function containsDuplicate($nums) {
        $array = [];
        foreach($nums as $num){
            if(isset($array[$num])) return true;
